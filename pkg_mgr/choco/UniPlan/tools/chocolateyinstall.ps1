eb2b9bd19ae65ea9b1ddaba88793225ef835baa0
David <157735322+David735453@users.noreply.github.com>
2026-05-17 11:30:50 +0300

    rename to UniPlan_Releases

--- pkg_mgr/choco/UniPlan/tools/chocolateyinstall.ps1
+++ pkg_mgr/choco/UniPlan/tools/chocolateyinstall.ps1
@@ -6,10 +6,10 @@ $os = Get-CimInstance -ClassName Win32_OperatingSystem
 $architecture = $os.OSArchitecture
 
 if ($architecture -match 'ARM') {
-    $url = 'https://github.com/dStudios-735453/UniPlan_releases/releases/download/v01.05.2026-0806/UniPlan_0.0.1_arm64-setup.exe'
+    $url = 'https://github.com/dStudios-735453/UniPlan_Releases/releases/download/v01.05.2026-0806/UniPlan_0.0.1_arm64-setup.exe'
     $checksum = '1ABBA177BD7988277662E4EC532F1DBE583124EA472E34E1397EBBC7239C2284'
 } else {
-    $url = 'https://github.com/dStudios-735453/UniPlan_releases/releases/download/v01.05.2026-0806/UniPlan_0.0.1_x64-setup.exe'
+    $url = 'https://github.com/dStudios-735453/UniPlan_Releases/releases/download/v01.05.2026-0806/UniPlan_0.0.1_x64-setup.exe'
     $checksum = '4BE92F168060C5BEC8A0835F1BADBB0B026F76087C8DE2AAA0FE0B3BE6C1529C'
 }
 
